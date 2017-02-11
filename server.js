7537041087aaf5f595d349ae6c0df865b64ba54d
Joe Chapman <contemno82@gmail.com>
2017-02-11 19:39:11 +0000

    removed api call until config set up

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/', function(req, res) {
 
 app.post('/dashboard-select', [
 //  calendarCall,
-  weatherCall,
+//  weatherCall,
   xkcdCall,
   function(req, res) {
     res.locals.dummyData = dummyData;
