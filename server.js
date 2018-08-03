eef6b6f2c9bc030950b3ed15d137af839183c8ac
joe.chapman <joe.chapman@dwp.gsi.gov.uk>
2018-08-03 11:17:40 +0100

    added https

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 /*jshint esversion: 6 */
 require('dotenv').config();
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 const app = express();
+const key = fs.readFileSync(path.join(__dirname, '/development/certs/server.key'));
+const cert = fs.readFileSync(path.join(__dirname, '/development/certs/server.crt'));
+const options = {key: key, cert: cert};
+
 require(__dirname + '/config')(app);
 require(__dirname + '/routes')(app);
 
 const PORT = process.env.PORT || 3000;
 
-// var string = "oHKkO3BHZ7qt6r7P2pEbGnzDB:fOkgB1yT5SMAHOcCWvAGowH7qSjrvHgc712iREdflN9XsclbfV";
-// var buffer = new Buffer(string);
-// var toBase64 = buffer.toString('base64');
-// console.log(string + " encoding to base64 is " + toBase64);
-
-app.listen(PORT, function() {
-  console.log('Server listening on port:' + PORT);
+https.createServer(options, app).listen(PORT, function () {
+    console.log('Express HTTPS server listening on port ' + PORT);
 });
 
 
